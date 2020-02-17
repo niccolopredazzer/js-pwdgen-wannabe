5894c8cd1b7b83c6a5bb2113ddf10d819f5ba11e
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-02-17 18:14:17 +0100

    completo

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt ('scrivi il tuo nome');
+var cognome = prompt ('scrivi il tuo cognome');
+var colore = prompt ('scrivi il tuo colore preferito');
+
+document.getElementById('password').innerHTML = nome + cognome + colore + '20';
